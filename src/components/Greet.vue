2c932aa413df27c292fa13c4b9d8e294aff06845
NRintaro <hemisphere.babylon@icloud.com>
2018-06-25 18:49:11 +0900

    ルートの追加

---
+++ src/components/Greet.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="greet">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Greet',
+  data () {
+    return {
+      msg: 'Greet Componet'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
