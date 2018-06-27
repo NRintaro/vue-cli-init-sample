f978dc3c6e56a2cbf68964dd926cb3ee495a5fee
NRintaro <hemisphere.babylon@icloud.com>
2018-06-27 17:41:11 +0900

    ネイティブのカメラロールを起動する為のmessageHandlersを追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>ネイティブへの処理</h2>
     <input type="button"
-      value="Webからネイティブにメッセージを送信"
+      value="メッセージを送信する"
       v-on:click="passDataToNaitive"
-      id="passDataToNaitive"
+      class="btn"
+    />
+    <input type="button"
+      value="カメラロールを表示する"
+      v-on:click="showAlbum"
+      class="btn"
     />
   </div>
 </template>
@@ -17,6 +23,10 @@ export default {
       let userAgent = window.navigator.userAgent
       window.webkit.messageHandlers
         .callbackHandler.postMessage(userAgent)
+    },
+    showAlbum: function () {
+      window.webkit.messageHandlers
+        .showAlbum.postMessage('hoge')
     }
   },
   data () {
@@ -33,7 +43,7 @@ h1 {
   font-weight: normal;
 }
 
-#passDataToNative {
+.btn {
   width: 200px;
   height: 100px;
   margin: 15px;
