5535d117a9cc278c09986623523045a1c7391d2d
NRintaro <hemisphere.babylon@icloud.com>
2018-06-28 19:07:49 +0900

    iOSから送られてくるbase64Urlを表示するためのページを作成

---
+++ src/components/Image.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <img id="img" src="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Image',
+  data () {
+    return {
+      msg: 'iOSから受け取った画像を表示する'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Greet from '@/components/Greet'
+import Image from '@/components/Image'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/greet',
       name: 'Greet',
       component: Greet
+    },
+    {
+      path: '/image',
+      name: 'Image',
+      component: Image
     }
   ]
 })
